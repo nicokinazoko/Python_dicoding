430a60023bf9f8d9a011ccfc99f3d0252f9ddc8d
nicokinazoko <nicodunois017157@gmail.com>
2020-10-11 19:06:31 +0700

    Create input_output.py

---
+++ input_output.py
@@ -0,0 +1,28 @@
+# Print
+print("Hello, World!")
+
+# Memasukkan nilai variabel pada string
+a = 5
+print('The value of a is', a)
+
+print('hello {}'.format('kakak'))
+
+name = "Nico"
+print("Hello, %s!" % name)
+
+name = "Nico"
+age = 22
+print("%s is %d years old." % (name, age))
+
+
+mylist = [1,2,3]
+print("A list: %s" % mylist)
+
+a, b = 10, 11
+a, b
+print('a: %x and b: %X' % (a, b))
+
+
+# Input
+num = input('Enter a number: ')
+num
